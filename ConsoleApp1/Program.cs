dad57ecde03bbe1b2e6e2361cde34fbf6b0f64ee
User <User@DESKTOP-LU6K5F4>
2024-02-02 10:55:20 +0000

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Helo, what is your name???");
+
+var name = Console.ReadLine();
+var date = DateTime.UtcNow;
+
+Console.WriteLine("---------------------------------");
+Console.WriteLine($"Welcome {name}. It's {date}, welcome to my math's game!\n");
+Console.WriteLine(@$"What kind of math's game would you like to play today?? Please choose from the following choices below:
+A - Addition Game
+S - Subtraction Game 
+M - Multipication Game
+D - Division Game
+Q - Quit the programe");
+Console.WriteLine("---------------------------------");
